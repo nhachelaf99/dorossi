eab2ced562552ea9e64b6d0dac1b93d051a28096
nhachelaf99 <102677595+nhachelaf99@users.noreply.github.com>
2022-04-09 02:01:28 +0100

    Add files via upload

--- dourousi/Dashboard/dashboard.js
+++ dourousi/Dashboard/dashboard.js
@@ -37,3 +37,41 @@ document.querySelectorAll('.nav-right a').forEach(n => n.addEventListener("click
     hamburger.classList.remove("active")
     navRight.classList.remove("active")
 }))
+
+
+//Dashboard
+const sideProfile = document.getElementById("dash-profile")
+const sideLessons = document.getElementById("dash-lessons")
+const sideSub = document.getElementById("dash-sub")
+const dashProfile = document.getElementById("dash-profile-content")
+const dashLessons = document.getElementById("dash-lessons-content")
+const dashSub = document.getElementById("dash-sub-content")
+
+sideProfile.addEventListener("click", ProfileClick)
+function ProfileClick() {
+    sideProfile.classList.add("active")
+    sideLessons.classList.remove("active")
+    sideSub.classList.remove("active")
+    dashProfile.classList.add("active")
+    dashLessons.classList.remove("active")
+    dashSub.classList.remove("active")
+}
+
+sideLessons.addEventListener("click", LessonsClick)
+function LessonsClick() {
+    sideLessons.classList.add("active")
+    sideProfile.classList.remove("active")
+    sideSub.classList.remove("active")
+    dashLessons.classList.add("active")
+    dashProfile.classList.remove("active")
+    dashSub.classList.remove("active")
+}
+
+sideSub.addEventListener("click", SideClick)
+function SideClick() {
+    sideSub.classList.add("active")
+    sideProfile.classList.remove("active")
+    sideLessons.classList.remove("active")
+}
+
+window.onload = ProfileClick()
